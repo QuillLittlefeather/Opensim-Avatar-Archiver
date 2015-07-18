e4081691bcba5f23ab48e18c3d019cdb179e6a5a
Quill Littlefeather <qlittlefeather@gmail.com>
2015-07-18 08:09:37 +0100

    Added License

--- addon-modules/QuillModules/AvatarArchive/Serializer.cs
+++ addon-modules/QuillModules/AvatarArchive/Serializer.cs
@@ -1,6 +1,6 @@
 ﻿/*
- * Copyright (c) Contributors, http://opensimulator.org/
- * See CONTRIBUTORS.TXT for a full list of copyright holders.
+ * Copyright (c) Quill Littlefeather, http://qlittlefeather.com
+ * 
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
